fc4e7d77c3ead4afa3819862a15a1dd718c620fb
Leonetienne <leonetienne@hotmail.de>
2021-03-21 16:54:44 +0100

    Project setup

---
+++ App/App.cpp
@@ -0,0 +1,10 @@
+// App.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
+void fnApp()
+{
+}
