40b10d70c1528e4647413b372c1e216bef0257b6
Leonetienne <leonetienne@hotmail.de>
2021-03-23 16:04:52 +0100

    Added new unit test for vector normalization

--- _Test_Tornado/Vector3.cpp
+++ _Test_Tornado/Vector3.cpp
@@ -593,6 +593,15 @@ namespace Vectors
 			return;
 		}
 
+		// Tests if an input vector of length 0 is handled correctly by the normalize method
+		TEST_METHOD(Normalize_Length_Before_Is_0)
+		{
+			Vector3d vec(0, 0, 0);
+			vec.Normalize();
+			Assert::AreEqual(0.0, vec.Magnitude());
+			return;
+		}
+
 		// Tests for any normalized vector to be of length 1
 		TEST_METHOD(Normalize_Length_Is_1)
 		{
@@ -602,7 +611,12 @@ namespace Vectors
 				double x = LARGE_RAND_DOUBLE;
 				double y = LARGE_RAND_DOUBLE;
 				double z = LARGE_RAND_DOUBLE;
+
 				Vector3d vec(x, y, z);
+
+				// Prevent a vector of length 0 going in
+				if (vec.SqrMagnitude() == 0)
+					vec.x++;
 
 				vec.Normalize();
 
