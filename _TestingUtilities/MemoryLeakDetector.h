2211297fb3999ce2d5b226408ea3a7890e5c00f7
Leonetienne <leonetienne@hotmail.de>
2021-03-25 14:20:06 +0100

    Removed tests from release builds that depend on memory insight (debug mode only)

--- _TestingUtilities/MemoryLeakDetector.h
+++ _TestingUtilities/MemoryLeakDetector.h
@@ -5,17 +5,33 @@ class MemoryLeakDetector
 {
 public:
 	// Defines the initial mem state to check against
-	void Init();
+	void Init()
+	{
+		_CrtMemCheckpoint(&stateInit);
+		return;
+	};
 
 	// Returns whether or not the process uses more memory than at the point when Init() was called
-	bool DetectLeak();
+	bool DetectLeak()
+	{
+		_CrtMemCheckpoint(&stateEnd);
+		return Evaluate();
+	};
 
 	// Returns the absolute memory difference since calling Init(). WARNING: The ABSOLUTE difference! No negative values!
-	int Difference();
+	int Difference()
+	{
+		// I know that it's the exact same code as in DetectedLeak().
+		_CrtMemCheckpoint(&stateEnd);
+		return Evaluate();
+	}
 
 
 private:
-	int Evaluate();
+	int Evaluate()
+	{
+		return _CrtMemDifference(&stateBfr, &stateInit, &stateEnd);
+	}
 
 	_CrtMemState stateInit;
 	_CrtMemState stateEnd;
