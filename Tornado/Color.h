b075da4bb356bb839fcf24e127c2f81ae3f2ad6a
Leonetienne <leonetienne@hotmail.de>
2021-03-25 15:54:28 +0100

    Changes to color struct and unit tests for it

--- Tornado/Color.h
+++ Tornado/Color.h
@@ -3,10 +3,15 @@
 
 struct Color
 {
+	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
+
 	uint8_t r = 0;
 	uint8_t g = 0;
 	uint8_t b = 0;
 	uint8_t a = 0;
+
+	bool operator==(const Color& other) const;
+	bool operator!=(const Color& other) const;
 
 	static const Color black;
 	static const Color gray;
@@ -18,13 +23,3 @@ struct Color
 	static const Color pink;
 	static const Color white;
 };
-
-const Color Color::black  = {   0,   0,   0, 255 };
-const Color Color::gray   = { 128, 128, 128, 255 };
-const Color Color::red    = { 255,   0,   0, 255 };
-const Color Color::green  = {   0, 255,   0, 255 };
-const Color Color::blue   = {   0,   0, 255, 255 };
-const Color Color::yellow = { 255, 255,   0, 255 };
-const Color Color::cyan   = {   0, 255, 255, 255 };
-const Color Color::pink   = { 255,   0, 255, 255 };
-const Color Color::white  = {  255, 255, 255, 255 };
---
+++ _Test_Tornado/Color.cpp
@@ -0,0 +1,135 @@
+#include "CppUnitTest.h"
+#include "../Tornado/Color.h"
+#include "../_TestingUtilities/HandyMacros.h"
+#include <random>
+#include <sstream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+// The Refit() tests cover the Refit() method, the PixelBuffer(uint8_t* existingData, Vector2i size) constructor, and the copy constructor!
+// They all use the Refit() method!
+
+namespace TexturingAndStuff
+{
+	TEST_CLASS(_Color)
+	{
+	private:
+		std::mt19937 rng;
+
+	public:
+		// Constructor
+		_Color()
+		{
+			rng = std::mt19937((std::random_device())());
+			return;
+		}
+
+		// Tests that a color does match the values given to the constructor
+		TEST_METHOD(Color_Does_Match_Constructor_Without_A)
+		{
+			// Test 100 times
+			for (std::size_t i = 0; i < 100; i++)
+			{
+				const uint8_t r = rng() % 255;
+				const uint8_t g = rng() % 255;
+				const uint8_t b = rng() % 255;
+
+				Color col(r, g, b);
+				Assert::AreEqual(r, col.r);
+				Assert::AreEqual(g, col.g);
+				Assert::AreEqual(b, col.b);
+				Assert::AreEqual(255, (int)col.a);
+			}
+
+			return;
+		}
+
+		// Tests that a color does match the values given to the constructor
+		TEST_METHOD(Color_Does_Match_Constructor)
+		{
+			// Test 100 times
+			for (std::size_t i = 0; i < 100; i++)
+			{
+				const uint8_t r = rng() % 255;
+				const uint8_t g = rng() % 255;
+				const uint8_t b = rng() % 255;
+				const uint8_t a = rng() % 255;
+
+				Color col(r, g, b, a);
+				// We could also use operator==, but this way we will know which channel failed.
+				Assert::AreEqual(r, col.r);
+				Assert::AreEqual(g, col.g);
+				Assert::AreEqual(b, col.b);
+				Assert::AreEqual(a, col.a);
+			}
+
+			return;
+		}
+
+		// Tests that the operator equals (==) works
+		TEST_METHOD(Operator_Equals)
+		{
+			// Test 100 times
+			for (std::size_t i = 0; i < 100; i++)
+			{
+				const uint8_t ar = rng() % 10;
+				const uint8_t ag = rng() % 10;
+				const uint8_t ab = rng() % 10;
+				const uint8_t aa = rng() % 10;
+				const uint8_t br = rng() % 10;
+				const uint8_t bg = rng() % 10;
+				const uint8_t bb = rng() % 10;
+				const uint8_t ba = rng() % 10;
+
+				const Color a(ar, ag, ab, aa);
+				const Color b(br, bg, bb, ba);
+
+				Assert::AreEqual(
+					(ar == br) && (ag == bg) && (ab == bb) && (aa == ba),
+					a == b
+				);
+			}
+		}
+
+		// Tests that the operator not-equals (!=) works
+		TEST_METHOD(Operator_NotEquals)
+		{
+			// Test 100 times
+			for (std::size_t i = 0; i < 100; i++)
+			{
+				const uint8_t ar = rng() % 10;
+				const uint8_t ag = rng() % 10;
+				const uint8_t ab = rng() % 10;
+				const uint8_t aa = rng() % 10;
+				const uint8_t br = rng() % 10;
+				const uint8_t bg = rng() % 10;
+				const uint8_t bb = rng() % 10;
+				const uint8_t ba = rng() % 10;
+
+				const Color a(ar, ag, ab, aa);
+				const Color b(br, bg, bb, ba);
+
+				Assert::AreEqual(
+					(ar != br) || (ag != bg) || (ab != bb) || (aa != ba),
+					a != b
+				);
+			}
+		}
+		
+		// Tests that the constant colors are correct
+		TEST_METHOD(Constants)
+		{
+			Assert::IsTrue(Color(  0,   0,   0, 255) == Color::black );
+			Assert::IsTrue(Color(128, 128, 128, 255) == Color::gray  );
+			Assert::IsTrue(Color(255,   0,   0, 255) == Color::red	 );
+			Assert::IsTrue(Color(  0, 255,   0, 255) == Color::green );
+			Assert::IsTrue(Color(  0,   0, 255, 255) == Color::blue  );
+			Assert::IsTrue(Color(255, 255,   0, 255) == Color::yellow);
+			Assert::IsTrue(Color(  0, 255, 255, 255) == Color::cyan  );
+			Assert::IsTrue(Color(255,   0, 255, 255) == Color::pink	 );
+			Assert::IsTrue(Color(255, 255, 255, 255) == Color::white );
+			
+			return;
+		}
+	};
+}
