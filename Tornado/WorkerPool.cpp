feb7e1827645659e3708dc76292b6b2ac08c3c84
Leonetienne <leonetienne@hotmail.de>
2021-03-26 12:01:42 +0100

    Added workerPool

---
+++ Tornado/WorkerPool.cpp
@@ -0,0 +1,147 @@
+#include "WorkerPool.h"
+
+WorkerPool::WorkerPool(std::size_t numWorkers)
+{
+	workers.reserve(numWorkers);
+
+	for (std::size_t i = 0; i < numWorkers; i++)
+	{
+		Worker* newWorker = new Worker();
+		newWorker->workerPool = this;
+		newWorker->ownThread = new std::thread(&Worker::Lifecycle, this);
+
+		workers.push_back(newWorker);
+	}
+
+	return;
+}
+
+void WorkerPool::QueueTask(WorkerTask* task)
+{
+	taskQueue.push_back(task);
+
+	return;
+}
+
+std::size_t WorkerPool::GetQueueLength() const
+{
+	std::size_t count = 0;
+
+	for (const WorkerTask* wt : taskQueue)
+		if (wt->state == WorkerTaskState::QUEUED)
+			count++;
+
+
+	return count;
+}
+
+std::size_t WorkerPool::GetNumWorkers() const
+{
+	return workers.size();
+}
+
+std::size_t WorkerPool::GetNumActiveWorkers() const
+{
+	std::size_t count = 0;
+
+	for (const Worker* w : workers)
+		if (!w->IsIdling())
+			count++;
+
+	return count;
+}
+
+void WorkerPool::Execute()
+{
+	// As long as we still have unassigned tasks to do
+	while (GetQueueLength() > 0)
+	{
+		for (Worker* w : workers)
+			if (w->IsIdling()) // And if a worker is not working
+				for (WorkerTask* wt : taskQueue)
+					if (wt->state == WorkerTaskState::QUEUED) // And if a task is unassigned
+						w->DoTask(wt);	// Add that task
+
+		_sleep(16); // Reduce cpu overhead
+	}
+
+	// Now all tasks are assigned. Wait for them to finish
+	for (const Worker* w : workers)
+		while (!w->IsIdling())
+			_sleep(16);
+
+	// Now all tasks are finished. Let's clean up after ourselves!
+	for (WorkerTask* wt : taskQueue)
+		delete wt;
+	taskQueue.clear();
+
+	return;
+}
+
+////////////////////////////////////////////////////////
+
+Worker::Worker()
+{
+	Lifecycle();
+	return;
+}
+
+Worker::~Worker()
+{
+	delete ownThread;
+	ownThread = nullptr;
+	return;
+}
+
+bool Worker::DoTask(WorkerTask* task)
+{
+	// Fail, if already working
+	if (!isIdling)
+		return false;
+
+	this->task = task;
+	isIdling = false;
+	doTask = true;
+
+	return;
+}
+
+bool Worker::IsIdling() const
+{
+	return isIdling;
+}
+
+void Worker::Stop()
+{
+	doStop = true;
+	return;
+}
+
+void Worker::Lifecycle()
+{
+	while (!doStop)
+	{
+		// Begin working on new task
+		if ((doTask) && (task != nullptr))
+		{
+			// Set flags for working
+			doTask = false;
+			isIdling = false;
+			task->state = WorkerTaskState::COMPUTING;
+
+			// Run task
+			task->task();
+
+			// Set flags for idling
+			isIdling = true;
+			task->state = WorkerTaskState::FINISHED;
+		}
+		// Idling
+		else
+		{
+			_sleep(16); // Reduce cpu load drastically when doing 'nothing'.
+		}
+	}
+
+	return;
+}
