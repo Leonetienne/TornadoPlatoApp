4f962bcbe3c804fafc023eec858ac631b807909a
Leonetienne <leonetienne@hotmail.de>
2021-04-12 21:47:17 +0200

    Increased window win resolution.

--- WinWindowDisplay/Implementation.cpp
+++ WinWindowDisplay/Implementation.cpp
@@ -1,4 +1,5 @@
 #include "Implementation.h"
+#include "renderres.h"
 #include <math.h>
 
 AppImplementation::AppImplementation(std::size_t width, std::size_t height)
@@ -22,12 +23,12 @@ void AppImplementation::Tick()
 {
 	plato->Update();
 
-	for (std::size_t x = 0; x < 800; x++)
-		for (std::size_t y = 0; y < 600; y++)
+	for (std::size_t x = 0; x < RENDER_WIDTH; x++)
+		for (std::size_t y = 0; y < RENDER_HEIGHT; y++)
 		{
-			uint8_t& r = bgrPixelBuffer[3 * (y * 800 + x) + 2];
-			uint8_t& g = bgrPixelBuffer[3 * (y * 800 + x) + 1];
-			uint8_t& b = bgrPixelBuffer[3 * (y * 800 + x) + 0];
+			uint8_t& r = bgrPixelBuffer[3 * (y * RENDER_WIDTH + x) + 2];
+			uint8_t& g = bgrPixelBuffer[3 * (y * RENDER_WIDTH + x) + 1];
+			uint8_t& b = bgrPixelBuffer[3 * (y * RENDER_WIDTH + x) + 0];
 
 			r = *plato->GetPixelBuffer()->GetPixel(Vector2i(x, y), 0);
 			g = *plato->GetPixelBuffer()->GetPixel(Vector2i(x, y), 1);
