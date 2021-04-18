83f10300f3ae58a52348558ff5ff4396a9644907
Leonetienne <leonetienne@hotmail.de>
2021-04-18 23:28:30 +0200

    Added new static test for plato

---
+++ _Test_StaticTestFixture_Plato/TransformHierarchy__Curl.cpp
@@ -0,0 +1,64 @@
+#include "TransformHierarchy__Curl.h"
+#include "Cube.h"
+#include "../Plato/Renderer.h"
+#include "../Plato/WorldObjectManager.h"
+#include "../Plato/bmplib.h"
+#include "Rotator.h"
+#include "Util.h"
+
+/*
+	This test tests the transform hierarchy resolving rotations.
+	It does this by "curling a finger".
+	It stacks a lot of cubes onto another and parents them in to each other. Each cubes lower cube is its parent.
+	Then it applies a local rotation to all cubes. They should curl in like a finger.
+
+*/
+
+Transform* TransformHierarchy__Curl::CreateLink(Transform* parent)
+{
+	WorldObject* wo = WorldObjectManager::NewWorldObject("joint", parent);
+	wo->AddComponent<MeshRenderer>(&mesh_coob, &mat_coob);
+	
+	wo->AddTag("joint");
+	wo->GetTransform()->Move(Vector3d::up * 3);
+
+	return wo->GetTransform();
+}
+
+TransformHierarchy__Curl::TransformHierarchy__Curl() :
+	TestFixture(__FUNCTION__), // Set the test fixtures name equal to the specialized class name (constructor function)
+	txt_coob(Color::black)
+{
+	// Reposition camera to a more fitting place
+	{
+		Transform* cam = WorldObjectManager::FindObjectById("main_camera_ypiv")->GetTransform();
+		cam->SetPosition(Vector3d(20, 10, 0));
+		cam->SetRotation(Quaternion::FromEuler(Vector3d(0, -90, 0)));
+	}
+
+	// Load cube mesh and texture
+	mesh_coob = Cube();
+	LoadTextureFromBmp(&txt_coob, "../Plato/Cube_furnace_gitignore_.bmp");
+	mat_coob.texture = &txt_coob;
+
+	// Create a root transform (for positioning and rotation)
+	Transform* jointRoot = WorldObjectManager::NewWorldObject("joint_root")->GetTransform();
+	jointRoot->Rotate(Quaternion::FromEuler(Vector3d::up * 180));
+	//jointRoot->Rotate(Quaternion::FromEuler(Vector3d::up * 0));
+
+	// Create five joints
+	Transform* lastParent = jointRoot;
+	for (std::size_t i = 0; i < 5; i++)
+		lastParent = CreateLink(lastParent);
+
+	return;
+}
+
+void TransformHierarchy__Curl::Update(double frametime)
+{
+	if (GetAsyncKeyState(VK_SPACE))
+		for (WorldObject* wo : WorldObjectManager::FindObjectsByTag("joint"))
+			wo->GetTransform()->Rotate(Quaternion::FromEuler(Vector3d::right * 0.1));
+
+	return;
+}
