3052e245087f1fba2df697fc93987fbabff17f60
Leonetienne <leonetienne@hotmail.de>
2021-04-18 23:28:16 +0200

    Minor improvements to plato static tests

--- _Test_StaticTestFixture_Plato/Util.h
+++ _Test_StaticTestFixture_Plato/Util.h
@@ -6,10 +6,10 @@
 inline void LoadTextureFromBmp(Texture* txt, const std::string& file)
 {
 	BMPlib::BMP bmp;
-	bmp.Read("../Plato/Cube_thielen_gitignore_.bmp");
+	bmp.Read(file);
 	bmp.ConvertTo(BMPlib::BMP::COLOR_MODE::RGBA);
 	
-	PixelBuffer<4> txtPxb(bmp.GetPixelBuffer(), Vector2i(bmp.GetWidth(), bmp.GetHeight()));
+	PixelBuffer<4> txtPxb(bmp.GetPixelBuffer(), Vector2i((int)bmp.GetWidth(), (int)bmp.GetHeight()));
 	txt->SetPixelBuffer(txtPxb);
 	
 	return;
