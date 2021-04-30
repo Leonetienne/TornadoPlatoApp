fcd80789568e952bdb4701487fb24c2530123f02
Leonetienne <leonetienne@hotmail.de>
2021-04-30 19:32:35 +0200

    Added skybox to objparser__load static test

--- _Test_StaticTestFixture_Plato/Test__OBJParser__Load.cpp
+++ _Test_StaticTestFixture_Plato/Test__OBJParser__Load.cpp
@@ -1,6 +1,7 @@
 #include "Test__OBJParser__Load.h"
 #include "../Plato/WorldObjectManager.h"
 #include "../Plato/ResourceManager.h"
+#include "Rotator.h"
 #include <iostream>
 
 Test__OBJParser__Load::Test__OBJParser__Load() :
@@ -10,15 +11,19 @@ Test__OBJParser__Load::Test__OBJParser__Load() :
 	ResourceManager::LoadMeshFromObj("bottle", "bottle.obj");
 	ResourceManager::LoadMeshFromObj("spoder", "spoder__gitignore_.obj");
 	ResourceManager::LoadMeshFromObj("character", "character__gitignore_.obj");
+	ResourceManager::LoadMeshFromObj("skybox", "skybox.obj");
 
 	ResourceManager::LoadTextureFromBmp("bottle", "bottle text.bmp");
 	ResourceManager::LoadTextureFromBmp("spoder", "spoder__gitignore_.bmp");
 	ResourceManager::LoadTextureFromBmp("character", "character__gitignore_.bmp");
+	ResourceManager::LoadTextureFromBmp("skybox", "example_skybox.bmp");
 
 	ResourceManager::NewMaterial("bottle")->texture = ResourceManager::FindTexture("bottle");
 	ResourceManager::NewMaterial("spoder")->texture = ResourceManager::FindTexture("spoder");
 	ResourceManager::NewMaterial("character")->texture = ResourceManager::FindTexture("character");
+	ResourceManager::NewMaterial("skybox")->texture = ResourceManager::FindTexture("skybox");
 
+	// Create spider
 	wo_mesh = WorldObjectManager::NewWorldObject("spoder");
 	wo_mesh->AddComponent<MeshRenderer>(
 		ResourceManager::FindMesh("spoder"),
@@ -31,8 +36,15 @@ Test__OBJParser__Load::Test__OBJParser__Load() :
 	wo_mesh->AddComponent<MeshRenderer>(
 		ResourceManager::FindMesh("character"),
 		ResourceManager::FindMaterial("character")
-	);
+		);
 
+	// Create skybox
+	Transform* skybox = WorldObjectManager::NewWorldObject("skybox")->transform;
+	skybox->worldObject->AddComponent<MeshRenderer>(
+		ResourceManager::FindMesh("skybox"),
+		ResourceManager::FindMaterial("skybox")
+	);
+	skybox->Scale(Vector3d::one * 50);
 
 	return;
 }
