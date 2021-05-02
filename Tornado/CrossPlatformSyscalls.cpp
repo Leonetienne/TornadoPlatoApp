35111694ef9c85b045ae98c24bb5b33a9a249605
Leonetienne <leonetienne@hotmail.de>
2021-05-02 18:32:20 +0200

    Added comment

--- Tornado/CrossPlatformSyscalls.cpp
+++ Tornado/CrossPlatformSyscalls.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #endif
 
+// I am not using std::this_thread::sleep_for() because this is supposed to work without the pthread lib
 void cpSleep(std::size_t ms)
 {
 	#ifdef WINDOWS
