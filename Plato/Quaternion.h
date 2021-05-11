7f367185401d21eb314bafb276142b88fc299686
Leonetienne <leonetienne@hotmail.de>
2021-05-11 23:00:52 +0200

    Minor fixes in Quaternion

---
+++ Plato/Quaternion.h
@@ -0,0 +1,94 @@
+#pragma once
+#include "../Tornado/Vector3.h"
+#include "../Tornado/Vector4.h"
+#include "../Tornado/Matrix4x4.h"
+
+/** 3D rotation representation
+*/
+class Quaternion
+{
+public:
+    Quaternion();
+
+    //! Constructs by these raw values
+    Quaternion(const Vector4d values);
+
+    //! Copies this existing Quaternion
+    Quaternion(const Quaternion& q);
+
+    //! Creates an quaternion from euler angles
+    Quaternion(const Vector3d eulerAngles);
+
+    ~Quaternion();
+
+    //! Copies
+    Quaternion operator= (const Quaternion& q);
+
+    //! Multiplies (applies)
+    Quaternion operator* (const Quaternion& q) const;
+
+    //! Divides (applies)
+    Quaternion operator/ (Quaternion& q) const;
+
+    //! Also multiplies
+    Quaternion& operator*= (const Quaternion& q);
+
+    //! Also divides
+    Quaternion& operator/= (const Quaternion& q);
+
+    //! Will transform a 3d point around its origin
+    Vector3d operator* (const Vector3d& p) const;
+
+    bool operator== (const Quaternion& q) const;
+
+    bool operator!= (const Quaternion& q) const;
+
+    Quaternion Inverse() const;
+
+    Quaternion Conjugate() const;
+
+    Quaternion UnitQuaternion() const;
+
+    //! Will rotate a vector by this quaternion
+    Vector3d RotateVector(const Vector3d& vec) const;
+
+    //! Will return euler angles representing this Quaternion's rotation
+    Vector3d ToEulerAngles() const;
+
+    //! Will return a rotation matrix representing this Quaternions rotation
+    Matrix4x4 ToRotationMatrix() const;
+
+    //! Will return the raw four-dimensional values
+    Vector4d GetRawValues() const;
+
+    //! Will return the value between two Quaternion's as another Quaternion
+    Quaternion AngleBetween(const Quaternion& other) const;
+
+    //! Will set the raw four-dimensional values
+    void SetRawValues(const Vector4d values);
+
+    //! Will return the lerp result between two quaternions
+    Quaternion Lerp(const Quaternion& other, double t) const;
+
+    friend std::ostream& operator<< (std::ostream& os, const Quaternion& q);
+    friend std::wostream& operator<< (std::wostream& os, const Quaternion& q);
+
+private:
+    //! Scales
+    Quaternion operator* (const double scale) const;
+    Quaternion& operator*= (const double scale);
+
+    //! Quaternion values
+    Vector4d v;
+
+    //! Will force a regenartion of the euler and matrix caches on further converter calls
+    void InvalidateCache();
+
+    // Caches for conversions
+    mutable bool isCacheUpToDate_euler = false;
+    mutable Vector3d eulerCache;
+
+    mutable bool isCacheUpToDate_matrix = false;
+    mutable Matrix4x4 matrixCache;
+
+};
