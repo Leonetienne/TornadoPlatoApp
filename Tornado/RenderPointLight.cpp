a0aae3bdab532905d95ce4bb3bb9481dd622f700
Leonetienne <leonetienne@hotmail.de>
2021-05-14 18:16:07 +0200

    Pointlights not respecting normals

--- Tornado/RenderPointLight.cpp
+++ Tornado/RenderPointLight.cpp
@@ -2,15 +2,21 @@
 
 Color RenderPointLight::GetColorIntensityFactors(const InterRenderTriangle* ird, const Vector3d& point) const
 {
-	const double sqrDistance = (position - point).SqrMagnitude();
+	const Vector3d deltaPos = position - point;
+	const double sqrDistance = (deltaPos).SqrMagnitude();
 
 	// Too far away.
 	if (intensity*13 < sqrDistance)
-		return Color(0, 0, 0, 0);
+		return Color::black;
+
+	// Angle of the face towards the point light
+	const double dot = (deltaPos.Normalize()).DotProduct(ird->surfaceNormalWs);
+	if (dot < 0) // Not facing the point light? No light for you! >:(
+		return Color::black;
 
 	// Point is in range
 	const double invSqrCoefficient = intensity / sqrDistance;
-	const double fac = std::min(invSqrCoefficient / 10, 1.0);
+	const double fac = std::min(invSqrCoefficient / 10, 1.0) * dot;
 
 	return Color(
 		color.r * fac,
