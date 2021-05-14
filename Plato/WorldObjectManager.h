35d71fef2ac12732a5e8bac28cde8f1c6ef37539
Leonetienne <leonetienne@hotmail.de>
2021-05-14 02:55:41 +0200

    Doxyfied documenting comments in WorldObjectManager

--- Plato/WorldObjectManager.h
+++ Plato/WorldObjectManager.h
@@ -6,44 +6,58 @@
 // TODO:
 // Optimize name, id, tag lookups via hashes
 
+/** Manager class for WorldObjects.
+* This is the only class able to instanciate WorldObject's and Transform's.  
+* This class will take complete care of them. When creating a new WorldObject, 
+* it will automatically create a new Transform alongside it, link them, call their Component's hook methods
+* and free their memory either during runtime or in teardown.  
+*   
+* IMPORTANT:  
+* Don NOT delete WorldObject and Transform instances yourself!! Don't do it! If you have to, for whatever reason, 
+* delete a WorldObject immediately, do it like this:
+* ```
+* wObject->Destroy();
+* WorldObjectManager::DeleteFlaggedObjects();
+* `` 
+*/
 class WorldObjectManager
 {
 public:
-	// Will create a new world object just by a parent
+	//! Will create a new world object just by a parent
 	static WorldObject* NewWorldObject(Transform* parent);
 
-	// Will create a new world object by an optional name and optional parent
+	//! Will create a new world object by an optional name and optional parent
 	static WorldObject* NewWorldObject(const std::string& name = "World Object", Transform* parent = nullptr);
 
-	// Will return a world object that goes by this id. Nullptr if none
+	//! Will return a world object that goes by this id. Nullptr if none
 	static WorldObject* FindObjectById(const std::string& id);
 
-	// Will search for world objects by their name. Only returns absolute matches.
+	//! Will search for world objects by their name. Only returns absolute matches.
 	static std::vector<WorldObject*> FindObjectsByName(const std::string& name);
 
-	// Will search for world objects by their associated tags.
+	//! Will search for world objects by their associated tags.
 	static std::vector<WorldObject*> FindObjectsByTag(const std::string& tag);
 
-	// Will return the number of world objects
+	//! Will return the number of world objects
 	static std::size_t GetNumObjects();
 
-	// Will look for objects marked to be deleted and will delete them.
-	// Call this once at the absolute beginning of every frame
+	//! Will look for objects marked to be deleted and will delete them.  
+	//! Call this once at the absolute beginning of every frame
 	static void DeleteFlaggedObjects();
 
-	// Will call the hook method "Update()" on all world objects.
-	// Only call this ONCE per frame in your main loop!!
+	//! Will call the hook method "Update()" on all world objects.  
+	//! Only call this ONCE per frame in your main loop!!
 	static void CallHook__Update(double frametime);
 
-	// Will call the hook method "LateUpdate()" on all world objects.
-	// Only call this ONCE per frame in your main loop!!
+	//! Will call the hook method "LateUpdate()" on all world objects.  
+	//! Only call this ONCE per frame in your main loop!!
 	static void CallHook__LateUpdate(double frametime);
 
-	// Will call the hook method "Update()" on all world objects.
-	// Only call this ONCE per frame in your main loop!!
+	//! Will call the hook method "Update()" on all world objects.  
+	//! Only call this ONCE per frame in your main loop!!
 	static void CallHook__Render(Renderer* renderer);
 
-	// Will free all world objects
+	//! Will free all world objects
 	static void Free();
 
 private:
