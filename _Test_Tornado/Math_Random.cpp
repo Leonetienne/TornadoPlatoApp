bdb643f0e59dac2f5f155ff60370c2e0383c46db
Leonetienne <leonetienne@hotmail.de>
2021-05-15 20:48:13 +0200

    Added unit tests for tornado/Math

---
+++ _Test_Tornado/Math_Random.cpp
@@ -0,0 +1,26 @@
+#include "CppUnitTest.h"
+#include "../Tornado/Math.h"
+#include <random>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace _Math
+{
+	TEST_CLASS(_Random)
+	{
+	public:
+		// Checks that all values are always 0 <= v <= 1
+		TEST_METHOD(Always_Satisfies_0_lt_v_lt_1)
+		{
+			// Test 1000 random values
+			for (std::size_t i = 0; i < 1e3; i++)
+			{
+				const double rnd = Math::Random();
+				Assert::IsTrue(rnd >= 0.0, L"rnd < 0");
+				Assert::IsTrue(rnd <= 1.0, L"rnd > 1");
+			}
+
+			return;
+		}
+	};
+}
