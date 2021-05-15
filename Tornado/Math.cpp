10d3ed1907790b1de9bd1224454639fa1aab3ee1
Leonetienne <leonetienne@hotmail.de>
2021-05-15 17:32:45 +0200

    Extended math utility class

--- Tornado/Math.cpp
+++ Tornado/Math.cpp
@@ -1,1 +1,66 @@
 #include "Math.h"
+#include <array>
+
+// Checks if the random number generator is initialized. Does nothing if it is, initializes if it isn't.
+#define MAKE_SURE_RNG_IS_INITIALIZED if (!isRngInitialized) InitRng();
+
+void Math::InitRng()
+{
+	// Create truly random source (from hardware events)
+	std::random_device randomSource;
+
+	// Generate enough truly random values to populate the entire state of the mersenne twister
+	std::array<int, std::mt19937::state_size> seedValues;
+	std::generate_n(seedValues.data(), seedValues.size(), std::ref(randomSource));
+	std::seed_seq seedSequence(seedValues.begin(), seedValues.end());
+
+	// Seed the mersenne twister with these values
+	rng = std::mt19937(seedSequence);
+
+	isRngInitialized = true;
+
+	return;
+}
+
+// Will return a random double between 0 and 1
+double Math::Random()
+{
+	MAKE_SURE_RNG_IS_INITIALIZED;
+
+	return (rng() % 6942069) / 6942069.0;
+}
+
+// Will return a random unsigned integer.
+unsigned int Math::RandomUint()
+{
+	MAKE_SURE_RNG_IS_INITIALIZED;
+
+	return rng();
+}
+
+// Will return a random integer
+unsigned int Math::RandomInt()
+{
+	MAKE_SURE_RNG_IS_INITIALIZED;
+
+	// Since this is supposed to return a random value anyways,
+	// we can let the random uint overflow without any problems.
+	return (int)rng();
+}
+
+// Will return a random double within a range  
+// These bounds are INCLUSIVE!
+double Math::RandomRange(double min, double max)
+{
+	return (Random() * (max - min)) + min;
+}
+
+// Will return a random integer within a range. This is faster than '(int)RandomRange(x,y)'
+// These bounds are INCLUSIVE!
+int Math::RandomIntRange(int min, int max)
+{
+	return (rng() % (max + 1 - min)) + min;
+}
+
+std::mt19937 Math::rng;
+bool Math::isRngInitialized = true;
--- Tornado/Math.h
+++ Tornado/Math.h
@@ -1,17 +1,61 @@
 #pragma once
+#include <random>
 
+/** Math utility class.
+*/
 class Math
 {
 public:
-	static constexpr double Max(double a, double b);
-	static constexpr double Min(double a, double b);
-	static constexpr double Clamp(double v, double min, double max);
-	static constexpr double Lerp(double a, double b, double t);
+	//! Will return the bigger of two values
+	[[nodiscard]] static constexpr double Max(const double a, const double b);
+	
+	//! Will return the smaller of two values
+	[[nodiscard]] static constexpr double Min(const double a, const double b);
+	
+	//! Will return 'v', but at least 'min', and at most 'max'
+	[[nodiscard]] static constexpr double Clamp(const double v, const double min, const double max);
+	
+	//! Will return the linear interpolation between 'a' and 'b' by 't'
+	[[nodiscard]] static constexpr double Lerp(double a, double b, double t);
+	
+	//! Will return the absolute value of 'a'
+	[[nodiscard]] static constexpr double Abs(const double a);
+
+	//! Compares two double values with a given accuracy
+	[[nodiscard]] static constexpr bool Similar(const double a, const double b, const double epsilon = 0.00001);
+
+	//! Will return a random double between 0 and 1
+	[[nodiscard]] static double Random();
+	
+	//! Will return a random unsigned integer.
+	[[nodiscard]] static unsigned int RandomUint();
+
+	//! Will return a random integer
+	[[nodiscard]] static unsigned int RandomInt();
+
+	//! Will return a random double within a range  
+	//! These bounds are INCLUSIVE!
+	[[nodiscard]] static double RandomRange(double min, double max);
+
+	//! Will return a random integer within a range. This is faster than '(int)RandomRange(x,y)'
+	//! These bounds are INCLUSIVE!
+	[[nodiscard]] static int RandomIntRange(int max, int min);
 
 private:
+	//! Will initialize the random number generator
+	static void InitRng();
+
+	static std::mt19937 rng;
+	static bool isRngInitialized;
+
 	// No instanciation! >:(
 	Math();
 };
+
+
+
+/*     These are just the inline methods. They have to lie in the header file.     */
+/*     The more sophisticated methods are in the .cpp					           */
 
 constexpr inline double Math::Max(double a, double b)
 {
@@ -32,4 +76,14 @@ constexpr inline double Math::Lerp(double a, double b, double t)
 {
 	double it = 1.0 - t;
 	return (a * it) + (b * t);
+}
+
+inline constexpr double Math::Abs(const double a)
+{
+	return (a > 0.0) ? a : -a;
+}
+
+inline constexpr bool Math::Math::Similar(const double a, const double b, const double epsilon)
+{
+	return Abs(a - b) <= epsilon;
 }
