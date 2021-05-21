f74fedfefd3610e8faf5c2ae5234a9902a7840b2
Leonetienne <leonetienne@hotmail.de>
2021-05-21 14:20:15 +0200

    Modified tornado/Math::Oscillate to return a or b on integer counters. depending on their 1-bit. Just normalization

--- Tornado/Constants.h
+++ Tornado/Constants.h
@@ -5,6 +5,9 @@
 //! Pi up to 50 decimal places
 #define PI 3.14159265358979323846264338327950288419716939937510
 
+//! Pi divided by two
+#define HALF_PI 1.57079632679489661923132169163975144209858469968755
+
 //! Factor to convert degrees to radians 
 #define Deg2Rad (PI / 180.0)
 
