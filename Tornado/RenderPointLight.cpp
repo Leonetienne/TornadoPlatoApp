ebf04ac1b0c3f29b1b02476ca3303efe5d5c504a
Leonetienne <leonetienne@hotmail.de>
2021-05-21 17:28:53 +0200

    Got prototype of lightsource bounding box working

--- Tornado/RenderPointLight.cpp
+++ Tornado/RenderPointLight.cpp
@@ -6,6 +6,9 @@ Color RenderPointLight::GetColorIntensityFactors(const InterRenderTriangle* ird,
 	const Vector3d deltaPos = position - point;
 	const double sqrDistance = deltaPos.SqrMagnitude();
 
+	if ((useBoundingBox) && (!Inside(point)))
+		return Color::black;
+
 	// Too far away.
 	if (intensityTimes255 <= sqrDistance)
 		return Color::black;
@@ -57,4 +60,45 @@ double RenderPointLight::GetHardlightFac(const double dot, const double invSqrCo
 double RenderPointLight::GetSoftlightFac(const double invSqrCoefficient) const
 {
 	return invSqrCoefficient;
+}
+
+double RenderPointLight::Dot(uint8_t side, const Vector3d& p) const
+{
+	switch (side)
+	{
+		// left
+	case 0:
+		return (box.blb - box.flb).CrossProduct(box.flt - box.flb).DotProduct(p - box.flb);
+	
+		// right
+	case 1:
+		return (box.frt - box.frb).CrossProduct(box.brb - box.frb).DotProduct(p - box.frb);
+	
+		// front
+	case 2:
+		return (box.flt - box.flb).CrossProduct(box.frb - box.flb).DotProduct(p - box.flb);
+	
+		// back
+	case 3:
+		return (box.brb - box.blb).CrossProduct(box.blt - box.blb).DotProduct(p - box.blb);
+	
+		// top
+	case 4:
+		return (box.blt - box.flt).CrossProduct(box.frt - box.flt).DotProduct(p - box.flt);
+	
+		// bottom
+	case 5:
+		return (box.frb - box.flb).CrossProduct(box.blb - box.flb).DotProduct(p - box.flb);
+	}
+
+	return 0.0;
+}
+
+bool RenderPointLight::Inside(const Vector3d& p) const
+{
+	for (std::size_t i = 0; i < 6; i++)
+		if (Dot(i, p) < 0)
+			return false;
+		
+	return true;
 }
