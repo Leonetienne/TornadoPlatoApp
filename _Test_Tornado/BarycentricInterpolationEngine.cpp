491640b07b9ee54df7134cdd97626c456b0848d0
Leon Etienne <github@leonetienne.de>
2024-10-09 16:24:34 +0200

    began migrating tornado tests

--- _Test_Tornado/BarycentricInterpolationEngine.cpp
+++ _Test_Tornado/BarycentricInterpolationEngine.cpp
@@ -1,164 +1,148 @@
-#include "CppUnitTest.h"
-#include "../Tornado/BarycentricInterpolationEngine.h"
-#include "Eule/Math.h"
+
+#include "../_TestingUtilities/Catch2.h"
 #include "../_TestingUtilities/HandyMacros.h"
+#include "../Tornado/BarycentricInterpolationEngine.h"
+#include "../Eule/Math.h"
 #include <random>
 #include <sstream>
 #include <chrono>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace TorGL;
 using Eule::Math;
 
-InterRenderTriangle GetExampleTriangle()
-{
-	InterRenderTriangle ird;
+namespace {
+    InterRenderTriangle GetExampleTriangle()
+    {
+        InterRenderTriangle ird;
 
-	// Triangle does not need to be clipped. Is known to be all visible
-	ird.c.pos_cs = { -325, 0.5, 1.1, 512 };
-	ird.b.pos_cs = { 412, 0.5, 1.1, 927 };
-	ird.a.pos_cs = { 0, 0.5, 1.1, 561 };
+        // Triangle does not need to be clipped. Is known to be all visible
+        ird.a.pos_cs = { -325, 0.5, 1.1, 512 };
+        ird.b.pos_cs = { 412, 0.5, 1.1, 927 };
+        ird.c.pos_cs = { 0, 0.5, 1.1, 561 };
 
-	// Perspective division
-	ird.a.pos_ndc = (ird.a.pos_cs / ird.a.pos_cs.w);
-	ird.b.pos_ndc = (ird.b.pos_cs / ird.b.pos_cs.w);
-	ird.c.pos_ndc = (ird.c.pos_cs / ird.c.pos_cs.w);
+        // Perspective division
+        ird.a.pos_ndc = (ird.a.pos_cs / ird.a.pos_cs.w);
+        ird.b.pos_ndc = (ird.b.pos_cs / ird.b.pos_cs.w);
+        ird.c.pos_ndc = (ird.c.pos_cs / ird.c.pos_cs.w);
 
-	// Transform to screen space
-	ird.a.pos_ss = ird.a.pos_ndc * 400 + Vector2d(400, 400);
-	ird.b.pos_ss = ird.b.pos_ndc * 400 + Vector2d(400, 400);
-	ird.c.pos_ss = ird.c.pos_ndc * 400 + Vector2d(400, 400);
+        // Transform to screen space
+        ird.a.pos_ss = ird.a.pos_ndc * 400 + Vector2d(400, 400);
+        ird.b.pos_ss = ird.b.pos_ndc * 400 + Vector2d(400, 400);
+        ird.c.pos_ss = ird.c.pos_ndc * 400 + Vector2d(400, 400);
 
-	return ird;
+        return ird;
+    }
 }
 
-namespace Engines
+// Tests that interpolation to a vertex does return that vertex's value
+TEST_CASE("Corners Remain Same", "[BarycentricInterpolation][Engine]")
 {
-	TEST_CLASS(_BarycentricInterpolationEngine)
-	{
-	private:
-		std::mt19937 rng;
+    InterRenderTriangle ird = GetExampleTriangle();
+    std::mt19937 rng((std::random_device())());
 
-	public:
-		// Constructor
-		_BarycentricInterpolationEngine()
-		{
-			rng = std::mt19937((std::random_device())());
-			return;
-		}
+    // Run interpolation test 100 times
+    for (std::size_t i = 0; i < 100; i++)
+    {
+        // Create vertex values
+        double a = LARGE_RAND_DOUBLE;
+        double b = LARGE_RAND_DOUBLE;
+        double c = LARGE_RAND_DOUBLE;
 
-		// Tests that interpolation to a vertex does return that vertices value
-		TEST_METHOD(Cornors_Remain_Same)
-		{
-			InterRenderTriangle ird = GetExampleTriangle();
+        // Interpolate value at position of a between a, b, and c
+        double ret = BarycentricInterpolationEngine::PerspectiveCorrected(
+            ird,
+            ird.a.pos_ss,
+            a,
+            b,
+            c
+        );
+        // Check the interpolated value is the exact value at a
+        REQUIRE(Math::Similar(a, ret));
 
-			// Run interpolation test 100 times
-			for (std::size_t i = 0; i < 100; i++)
-			{
-				// Create vertex values
-				double a = LARGE_RAND_DOUBLE;
-				double b = LARGE_RAND_DOUBLE;
-				double c = LARGE_RAND_DOUBLE;
+        // Interpolate value at position of b between a, b, and c
+        ret = BarycentricInterpolationEngine::PerspectiveCorrected(
+            ird,
+            ird.b.pos_ss,
+            a,
+            b,
+            c
+        );
+        // Check the interpolated value is the exact value at b
+        REQUIRE(Math::Similar(b, ret));
 
-				// Interpolate value at position of a between a, b and c
-				double ret = BarycentricInterpolationEngine::PerspectiveCorrected(
-					ird,
-					ird.a.pos_ss,
-					a,
-					b,
-					c
-				);
-				// Check the interpolated value is the exact value at a
-				Assert::IsTrue(Math::Similar(a, ret), L"Failed A");
-
-				// Interpolate value at position of b between a, b and c
-				ret = BarycentricInterpolationEngine::PerspectiveCorrected(
-					ird,
-					ird.b.pos_ss,
-					a,
-					b,
-					c
-				);
-				// Check the interpolated value is the exact value at a
-				Assert::IsTrue(Math::Similar(b, ret), L"Failed B");
-
-				// Interpolate value at position of c between a, b and c
-				ret = BarycentricInterpolationEngine::PerspectiveCorrected(
-					ird,
-					ird.c.pos_ss,
-					a,
-					b,
-					c
-				);
-				// Check the interpolated value is the exact value at a
-				Assert::IsTrue(Math::Similar(c, ret), L"Failed C");
-			}
-
-			return;
-		}
-
-		// Tests that using the cached variant is faster when interpolating multiple attributes per pixel
-		TEST_METHOD(Cached_Values_Are_Faster)
-		{
-			InterRenderTriangle ird = GetExampleTriangle();
-
-			// Benchmark without caching
-			std::chrono::nanoseconds nocache;
-			{
-				auto begin = std::chrono::high_resolution_clock::now();
-
-				// Interpolate many attributes on the same pixel
-				for (std::size_t i = 0; i < 10000; i++)
-				{
-					// Don't optimize shit
-					volatile double a = LARGE_RAND_DOUBLE;
-					volatile double b = LARGE_RAND_DOUBLE;
-					volatile double c = LARGE_RAND_DOUBLE;
-					volatile double ret = BarycentricInterpolationEngine::PerspectiveCorrected(
-						ird,
-						ird.a.pos_ss,
-						a,
-						b,
-						c
-					);
-				}
-				
-				auto end = std::chrono::high_resolution_clock::now();
-				nocache = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(end - begin));
-			}
-
-			// Benchmark with caching
-			std::chrono::nanoseconds cached;
-			{
-				auto begin = std::chrono::high_resolution_clock::now();
-
-				// Interpolate many attributes on the same pixel
-				std::array<double, 5> cache { 0 };
-				for (std::size_t i = 0; i < 10000; i++)
-				{
-					// Don't optimize shit
-					volatile double a = LARGE_RAND_DOUBLE;
-					volatile double b = LARGE_RAND_DOUBLE;
-					volatile double c = LARGE_RAND_DOUBLE;
-					volatile double ret = BarycentricInterpolationEngine::PerspectiveCorrect__CachedValues(
-						ird,
-						ird.a.pos_ss,
-						a,
-						b,
-						c,
-						&cache
-					);
-				}
-
-				auto end = std::chrono::high_resolution_clock::now();
-				cached = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(end - begin));
-			}
-
-			std::wstringstream wss;
-			wss << L"Wtf, no cached values was faster than with cached values! NoCache: " << nocache.count() << L"ns | Cached: " << cached.count() << L"ns." << std::endl;
-			Assert::IsTrue(nocache > cached, wss.str().c_str());
-
-			return;
-		}
-	};
+        // Interpolate value at position of c between a, b, and c
+        ret = BarycentricInterpolationEngine::PerspectiveCorrected(
+            ird,
+            ird.c.pos_ss,
+            a,
+            b,
+            c
+        );
+        // Check the interpolated value is the exact value at c
+        REQUIRE(Math::Similar(c, ret));
+    }
 }
+
+// Tests that using the cached variant is faster when interpolating multiple attributes per pixel
+TEST_CASE("Cached Values Are Faster", "[BarycentricInterpolation][Engine]")
+{
+    InterRenderTriangle ird = GetExampleTriangle();
+    std::mt19937 rng((std::random_device())());
+
+    // Benchmark without caching
+    std::chrono::nanoseconds nocache;
+    {
+        auto begin = std::chrono::high_resolution_clock::now();
+
+        // Interpolate many attributes on the same pixel
+        for (std::size_t i = 0; i < 10000; i++)
+        {
+            // Don't optimize shit
+            volatile double a = LARGE_RAND_DOUBLE;
+            volatile double b = LARGE_RAND_DOUBLE;
+            volatile double c = LARGE_RAND_DOUBLE;
+            volatile double ret = BarycentricInterpolationEngine::PerspectiveCorrected(
+                ird,
+                ird.a.pos_ss,
+                a,
+                b,
+                c
+            );
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        nocache = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    }
+
+    // Benchmark with caching
+    std::chrono::nanoseconds cached;
+    {
+        auto begin = std::chrono::high_resolution_clock::now();
+
+        // Interpolate many attributes on the same pixel
+        std::array<double, 5> cache { 0 };
+        for (std::size_t i = 0; i < 10000; i++)
+        {
+            // Don't optimize shit
+            volatile double a = LARGE_RAND_DOUBLE;
+            volatile double b = LARGE_RAND_DOUBLE;
+            volatile double c = LARGE_RAND_DOUBLE;
+            volatile double ret = BarycentricInterpolationEngine::PerspectiveCorrect__CachedValues(
+                ird,
+                ird.a.pos_ss,
+                a,
+                b,
+                c,
+                &cache
+            );
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        cached = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    }
+
+    std::wstringstream wss;
+    wss << L"Wtf, no cached values was faster than with cached values! NoCache: " << nocache.count() << L"ns | Cached: " << cached.count() << L"ns.";
+    REQUIRE(nocache > cached);
+}
+
--- _Test_Tornado/Color.cpp
+++ _Test_Tornado/Color.cpp
@@ -1,233 +1,183 @@
-#include "CppUnitTest.h"
-#include "../Tornado/Color.h"
+#include "../_TestingUtilities/Catch2.h"
+#include "../_TestingUtilities/Testutil.h"
 #include "../_TestingUtilities/HandyMacros.h"
-#include <random>
+#include "../Tornado/Color.h"
 #include <sstream>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace TorGL;
 
-namespace TexturingAndStuff
-{
-	TEST_CLASS(_Color)
-	{
-	private:
-		std::mt19937 rng;
-
-	public:
-		// Constructor
-		_Color()
-		{
-			rng = std::mt19937((std::random_device())());
-			return;
-		}
-
-		// Tests that if constructed with the default constructor, that all values are 0
-		TEST_METHOD(Default_Constructor_All_0)
-		{
-			// Test 100 times
-			for (std::size_t i = 0; i < 100; i++)
-			{
-				Color color;
-				Assert::AreEqual(0, (int)color.r, L"R");
-				Assert::AreEqual(0, (int)color.g, L"G");
-				Assert::AreEqual(0, (int)color.b, L"B");
-				Assert::AreEqual(0, (int)color.a, L"A");
-			}
-
-			return;
-		}
-
-		// Tests that a color does match the values given to the constructor
-		TEST_METHOD(Color_Does_Match_Constructor_Without_Alpha)
-		{
-			// Test 100 times
-			for (std::size_t i = 0; i < 100; i++)
-			{
-				const double r = rng() % 255;
-				const double g = rng() % 255;
-				const double b = rng() % 255;
-
-				Color col(r, g, b);
-				Assert::AreEqual(r, col.r);
-				Assert::AreEqual(g, col.g);
-				Assert::AreEqual(b, col.b);
-				Assert::AreEqual(255, (int)col.a);
-			}
-
-			return;
-		}
-
-		// Tests that a color does match the values given to the constructor
-		TEST_METHOD(Color_Does_Match_Constructor)
-		{
-			// Test 100 times
-			for (std::size_t i = 0; i < 100; i++)
-			{
-				const double r = rng() % 255;
-				const double g = rng() % 255;
-				const double b = rng() % 255;
-				const double a = rng() % 255;
-
-				Color col(r, g, b, a);
-				// We could also use operator==, but this way we will know which channel failed.
-				Assert::AreEqual(r, col.r);
-				Assert::AreEqual(g, col.g);
-				Assert::AreEqual(b, col.b);
-				Assert::AreEqual(a, col.a);
-			}
-
-			return;
-		}
-
-		// Tests expected lerp result for t=0.00
-		TEST_METHOD(Lerp_Expected_000)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			Assert::IsTrue(a == Color::Lerp(a, b, 0.00));
-
-			return;
-		}
-
-		// Tests expected lerp result for t=0.25
-		TEST_METHOD(Lerp_Expected_025)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			Color res = Color::Lerp(a, b, 0.25);
-			std::wstringstream wss;
-			wss << res;
-			Assert::IsTrue(Color(25, 175, 62.5, 50) == res, wss.str().c_str());
-
-			return;
-		}
-
-		// Tests expected lerp result for t=0.50
-		TEST_METHOD(Lerp_Expected_050)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			Color res = Color::Lerp(a, b, 0.50);
-			std::wstringstream wss;
-			wss << res;
-			Assert::IsTrue(Color(50, 150, 75, 100) == res, wss.str().c_str());
-
-			return;
-		}
-
-		// Tests expected lerp result for t=0.75
-		TEST_METHOD(Lerp_Expected_075)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			Color res = Color::Lerp(a, b, 0.75);
-			std::wstringstream wss;
-			wss << res;
-			Assert::IsTrue(Color(75, 125, 87.5, 150) == res, wss.str().c_str());
-
-			return;
-		}
-
-		// Tests expected lerp result for t=1.00
-		TEST_METHOD(Lerp_Expected_100)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			Assert::IsTrue(b == Color::Lerp(a, b, 1.00));
-
-			return;
-		}
-
-		// Tests lerp as a memberfunction
-		TEST_METHOD(Lerp_Memberfunction)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			Assert::IsTrue(Color(75, 125, 87.5, 150) == a.Lerp(b, 0.75));
-		}
-
-		// Tests lerpSelf
-		TEST_METHOD(LerpSelf)
-		{
-			Color a(0, 200, 50, 0);
-			Color b(100, 100, 100, 200);
-
-			a.LerpSelf(b, 0.75);
-
-			Assert::IsTrue(Color(75, 125, 87.5, 150) == a);
-		}
-
-		// Tests that the operator equals (==) works
-		TEST_METHOD(Operator_Equals)
-		{
-			// Test 1000 times
-			for (std::size_t i = 0; i < 1000; i++)
-			{
-				const double ar = rng() % 5;
-				const double ag = rng() % 5;
-				const double ab = rng() % 5;
-				const double aa = rng() % 5;
-				const double br = rng() % 5;
-				const double bg = rng() % 5;
-				const double bb = rng() % 5;
-				const double ba = rng() % 5;
-
-				const Color a(ar, ag, ab, aa);
-				const Color b(br, bg, bb, ba);
-
-				Assert::AreEqual(
-					(ar == br) && (ag == bg) && (ab == bb) && (aa == ba),
-					a == b
-				);
-			}
-		}
-
-		// Tests that the operator not-equals (!=) works
-		TEST_METHOD(Operator_NotEquals)
-		{
-			// Test 1000 times
-			for (std::size_t i = 0; i < 1000; i++)
-			{
-				const double ar = rng() % 5;
-				const double ag = rng() % 5;
-				const double ab = rng() % 5;
-				const double aa = rng() % 5;
-				const double br = rng() % 5;
-				const double bg = rng() % 5;
-				const double bb = rng() % 5;
-				const double ba = rng() % 5;
-
-				const Color a(ar, ag, ab, aa);
-				const Color b(br, bg, bb, ba);
-
-				Assert::AreEqual(
-					(ar != br) || (ag != bg) || (ab != bb) || (aa != ba),
-					a != b
-				);
-			}
-		}
-		
-		// Tests that the constant colors are correct
-		TEST_METHOD(Constants)
-		{
-			Assert::IsTrue(Color(  0,   0,   0, 255) == Color::black );
-			Assert::IsTrue(Color(128, 128, 128, 255) == Color::gray  );
-			Assert::IsTrue(Color(255,   0,   0, 255) == Color::red	 );
-			Assert::IsTrue(Color(  0, 255,   0, 255) == Color::green );
-			Assert::IsTrue(Color(  0,   0, 255, 255) == Color::blue  );
-			Assert::IsTrue(Color(255, 255,   0, 255) == Color::yellow);
-			Assert::IsTrue(Color(  0, 255, 255, 255) == Color::cyan  );
-			Assert::IsTrue(Color(255,   0, 255, 255) == Color::pink	 );
-			Assert::IsTrue(Color(255, 255, 255, 255) == Color::white );
-			
-			return;
-		}
-	};
+namespace {
+    static std::mt19937 rng = std::mt19937((std::random_device())());
 }
+
+TEST_CASE(__FILE__"/Default_Constructor_All_0", "[Color]")
+{
+    // Test 100 times
+    for (std::size_t i = 0; i < 100; i++)
+    {
+        Color color;
+        REQUIRE(color.r == 0);
+        REQUIRE(color.g == 0);
+        REQUIRE(color.b == 0);
+        REQUIRE(color.a == 0);
+    }
+}
+
+TEST_CASE("Color_Does_Match_Constructor_Without_Alpha", "[Color]")
+{
+    // Test 100 times
+    for (std::size_t i = 0; i < 100; i++)
+    {
+        const double r = rng() % 255;
+        const double g = rng() % 255;
+        const double b = rng() % 255;
+
+        Color col(r, g, b);
+        REQUIRE(col.r == r);
+        REQUIRE(col.g == g);
+        REQUIRE(col.b == b);
+        REQUIRE(col.a == 255);
+    }
+}
+
+TEST_CASE("Color_Does_Match_Constructor", "[Color]")
+{
+    // Test 100 times
+    for (std::size_t i = 0; i < 100; i++)
+    {
+        const double r = rng() % 255;
+        const double g = rng() % 255;
+        const double b = rng() % 255;
+        const double a = rng() % 255;
+
+        Color col(r, g, b, a);
+        REQUIRE(col.r == r);
+        REQUIRE(col.g == g);
+        REQUIRE(col.b == b);
+        REQUIRE(col.a == a);
+    }
+}
+
+TEST_CASE("Lerp_Expected_000", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+    REQUIRE(a == Color::Lerp(a, b, 0.00));
+}
+
+TEST_CASE("Lerp_Expected_025", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+
+    Color res = Color::Lerp(a, b, 0.25);
+    std::ostringstream oss;
+    oss << res;
+    REQUIRE(Color(25, 175, 62.5, 50) == res);
+}
+
+TEST_CASE("Lerp_Expected_050", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+
+    Color res = Color::Lerp(a, b, 0.50);
+    std::ostringstream oss;
+    oss << res;
+    REQUIRE(Color(50, 150, 75, 100) == res);
+}
+
+TEST_CASE("Lerp_Expected_075", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+
+    Color res = Color::Lerp(a, b, 0.75);
+    std::ostringstream oss;
+    oss << res;
+    REQUIRE(Color(75, 125, 87.5, 150) == res);
+}
+
+TEST_CASE("Lerp_Expected_100", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+    REQUIRE(b == Color::Lerp(a, b, 1.00));
+}
+
+TEST_CASE("Lerp_Memberfunction", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+    REQUIRE(Color(75, 125, 87.5, 150) == a.Lerp(b, 0.75));
+}
+
+TEST_CASE("LerpSelf", "[Color]")
+{
+    Color a(0, 200, 50, 0);
+    Color b(100, 100, 100, 200);
+    a.LerpSelf(b, 0.75);
+    REQUIRE(Color(75, 125, 87.5, 150) == a);
+}
+
+TEST_CASE("Operator_Equals", "[Color]")
+{
+    // Test 1000 times
+    for (std::size_t i = 0; i < 1000; i++)
+    {
+        const double ar = rng() % 5;
+        const double ag = rng() % 5;
+        const double ab = rng() % 5;
+        const double aa = rng() % 5;
+        const double br = rng() % 5;
+        const double bg = rng() % 5;
+        const double bb = rng() % 5;
+        const double ba = rng() % 5;
+
+        const Color a(ar, ag, ab, aa);
+        const Color b(br, bg, bb, ba);
+
+        // If all color values are equal, the color objects should be equal aswell
+        if ((ar == br) && (ag == bg) && (ab == bb) && (aa == ba)) {
+            REQUIRE(a == b);
+        }
+    }
+}
+
+TEST_CASE("Operator_NotEquals", "[Color]")
+{
+    // Test 1000 times
+    for (std::size_t i = 0; i < 1000; i++)
+    {
+        const double ar = rng() % 5;
+        const double ag = rng() % 5;
+        const double ab = rng() % 5;
+        const double aa = rng() % 5;
+        const double br = rng() % 5;
+        const double bg = rng() % 5;
+        const double bb = rng() % 5;
+        const double ba = rng() % 5;
+
+        const Color a(ar, ag, ab, aa);
+        const Color b(br, bg, bb, ba);
+
+        // If any color value is uneuqal, the color bject should also be unequal
+        if ((ar != br) || (ag != bg) || (ab != bb) || (aa != ba)) {
+            REQUIRE(a != b);
+        }
+    }
+}
+
+TEST_CASE("Constants", "[Color]")
+{
+    REQUIRE(Color(0, 0, 0, 255) == Color::black);
+    REQUIRE(Color(128, 128, 128, 255) == Color::gray);
+    REQUIRE(Color(255, 0, 0, 255) == Color::red);
+    REQUIRE(Color(0, 255, 0, 255) == Color::green);
+    REQUIRE(Color(0, 0, 255, 255) == Color::blue);
+    REQUIRE(Color(255, 255, 0, 255) == Color::yellow);
+    REQUIRE(Color(0, 255, 255, 255) == Color::cyan);
+    REQUIRE(Color(255, 0, 255, 255) == Color::pink);
+    REQUIRE(Color(255, 255, 255, 255) == Color::white);
+}
+
