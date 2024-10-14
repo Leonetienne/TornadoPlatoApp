620f897848d1b09ec8342d5d056f6df7425b708e
Leon Etienne <github@leonetienne.de>
2024-10-15 01:36:20 +0200

    fix bad commens

--- Tornado/Vector4.h
+++ Tornado/Vector4.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // This file is just an alias for Eule's Vector files.
-// It would be very tedios having to include "../Eule/Vector4.h", and then use Eule::Vector every time you want to use vectors
+// It would be very tedious having to include "../Eule/Vector4.h", and then use Eule::Vector every time you want to use vectors
 // within the renderer. Much more convenient to just include "Vector4.h".
-// Also, when using the game engine, one should ideally not have to directly interact with the maths engine at all.
+// Also, when using the graphics engine, one should ideally not have to directly interact with the maths engine at all.
 
 #include "../Eule/Vector4.h"
 
