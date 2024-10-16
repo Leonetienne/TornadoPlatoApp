e92793563993602b12cdd5ee1254ae24cdef6c0a
Leon Etienne <me@leonetienne.de>
2024-10-16 14:12:30 +0200

    define nixos build env

---
+++ _TestappSDL2/shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.llvmPackages.stdenv.mkDerivation {
+  name = "some-name";
+  nativeBuildInputs = with pkgs;[ cmake pkg-config ];
+  buildInputs = with pkgs;[
+	SDL2
+	SDL2_ttf
+	SDL2_net
+	SDL2_gfx
+	SDL2_sound
+	SDL2_mixer
+	SDL2_image
+  ];
+} 
