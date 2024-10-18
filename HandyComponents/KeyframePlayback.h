5141b2889f04842b14a45140b536221a97afb914
Tim Aldinger <tim.aldinger04@gmail.com>
2024-10-18 23:35:24 +0200

    added keyframe .h files

---
+++ HandyComponents/KeyframePlayback.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../Plato/Component.h"
+using namespace Plato;
+
+class KeyframePlayback : public Component {
+public:
+protected:
+private:
+};
---
+++ HandyComponents/KeyframeRecorder.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../Plato/Component.h"
+using namespace Plato;
+
+class KeyframeRecorder : public Component {
+public:
+protected:
+private:
+};
