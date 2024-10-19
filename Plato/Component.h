003d5873437a6a34f3be79e03288a2b187699d1e
Tim Aldinger <tim.aldinger04@gmail.com>
2024-10-19 20:21:01 +0200

    I forgor

--- Plato/Component.h
+++ Plato/Component.h
@@ -1,67 +1,72 @@
 #pragma once
 #include "Transform.h"
 
-namespace Plato
-{
-	class WorldObject;
-	class Renderer;
+namespace Plato {
+class WorldObject;
+class Renderer;
 
-	/** Base class for component-scripts that can be added to world objects.
-	*/
-	class Component
-	{
-	public:
-		//! Handle to the corresponding world object
-		WorldObject* const worldObject;
-		//! Handle to the corresponding transform
-		Transform* const transform;
+/** Base class for component-scripts that can be added to world objects.
+ */
+class Component {
+public:
+  //! Handle to the corresponding world object
+  WorldObject *const worldObject;
+  //! Handle to the corresponding transform
+  Transform *const transform;
 
-		//! Will return whether or not this component is locally enabled
-		bool GetIsEnabled() const;
+  //! Will return whether or not this component is locally enabled
+  bool GetIsEnabled() const;
 
-		//! Will return whether or not this component is globally enabled (respecting its world objects and its parents disabled-states)
-		bool GetIsGloballyEnabled() const;
+  //! Will return whether or not this component is globally enabled (respecting
+  //! its world objects and its parents disabled-states)
+  bool GetIsGloballyEnabled() const;
 
-		//! Will set the enabled-status of this component. Disabled components receive no hook calls!
-		void SetIsEnabled(bool state);
+  //! Will set the enabled-status of this component. Disabled components receive
+  //! no hook calls!
+  void SetIsEnabled(bool state);
 
-		//! Will locally enable this component. Disabled components receive no hook calls!
-		void Enable();
+  //! Will locally enable this component. Disabled components receive no hook
+  //! calls!
+  void Enable();
 
-		//! Will locally disable this component. Disabled components receive no hook calls!
-		void Disable();
+  //! Will locally disable this component. Disabled components receive no hook
+  //! calls!
+  void Disable();
 
-	protected:
-		virtual ~Component() {};
+protected:
+  virtual ~Component(){};
 
-		// ### GAME ENGINE HOOKS ### //
+  // ### GAME ENGINE HOOKS ### //
 
-		// These hooks are not defined as abstract ( = 0 ), because they should not have
-		// to be implemented. A component that just uses Update(double) should be fine by just implementing Update(double).
+  // These hooks are not defined as abstract ( = 0 ), because they should not
+  // have to be implemented. A component that just uses Update(double) should be
+  // fine by just implementing Update(double).
 
-		//! Gets called once after the object is all set up
-		virtual void Init() {};
+  //! Gets called once after the object is all set up
+  virtual void Init() {};
 
-		//! Gets called every frame
-		virtual void Update(double frametime) {};
+  //! Gets called every frame
+  virtual void Update(double frametime) {};
 
-		//! Gets called every frame, right after Update() has been called on every Component
-		virtual void LateUpdate(double frametime) {};
+  //! Gets called every frame, right after Update() has been called on every
+  //! Component
+  virtual void LateUpdate(double frametime) {};
 
-		//! Gets called every frame just before rendering Register your Renderables in renderer
-		virtual void Render(Renderer* renderer) {};
+  //! Gets called every frame just before rendering Register your Renderables in
+  //! renderer
+  virtual void Render(Renderer *renderer) {};
 
-		//! Gets called just before this object gets deleted
-		virtual void OnDestroy() {};
+  //! Gets called just before this object gets deleted
+  virtual void OnDestroy() {};
 
-	protected:
-		Component(WorldObject* worldObject);
+protected:
+  Component(WorldObject *worldObject);
 
-	private:
-		bool enabled = true;
+private:
+  bool enabled = true;
 
-		friend class WorldObject;
-		// Allow access to call hooks
-		friend class WorldObjectManager;
-	};
-}
+  friend class WorldObject;
+  // Allow access to call hooks
+  friend class WorldObjectManager;
+};
+} // namespace Plato
--- Scenes/Fun/Dust2/Dust2Scene.cpp
+++ Scenes/Fun/Dust2/Dust2Scene.cpp
@@ -1,69 +1,73 @@
 #include "Dust2Scene.h"
-#include "../../../Prefabs/Skybox/SkyboxPrefab.h"
+#include "../../../Plato/Application.h"
+#include "../../../Plato/Keyboard.h"
 #include "../../../Plato/MTLParser.h"
+#include "../../../Plato/MeshRenderer.h"
 #include "../../../Plato/PointLight.h"
 #include "../../../Plato/WorldObjectManager.h"
-#include "../../../Plato/MeshRenderer.h"
+#include "../../../Prefabs/Skybox/SkyboxPrefab.h"
 
 using namespace Plato;
 
 /*
-	Expected behaviour: See dust2 map, it generates materials automatically from an mtl file.
+        Expected behaviour: See dust2 map, it generates materials automatically
+   from an mtl file.
 */
-namespace {
-}
+namespace {}
 
-Dust2Scene::Dust2Scene() : Scene(__FUNCTION__) // Set the test fixtures name
+Dust2Scene::Dust2Scene()
+    : Scene(__FUNCTION__) // Set the test fixtures name
 {
-    // Create skybox
-    (SkyboxPrefab()).Instantiate();
+  // Create skybox
+  (SkyboxPrefab()).Instantiate();
 
-    const std::string assetsDir = "../../Scenes/Fun/Dust2/assets";
+  const std::string assetsDir = "../../Scenes/Fun/Dust2/assets";
 
-    // Load dust2 materials
-    MTLParser mtlParser;
-    mtlParser.ParseMtl(assetsDir+"/dust2.mtl", assetsDir);
+  // Load dust2 materials
+  MTLParser mtlParser;
+  mtlParser.ParseMtl(assetsDir + "/dust2.mtl", assetsDir);
 
-    // Load dust2 model
-    OBJParser OBJParser;
-    dust2Mesh = new Mesh;
-    *dust2Mesh = OBJParser.ParseObj(assetsDir+"/dust2.obj", true);
+  // Load dust2 model
+  OBJParser OBJParser;
+  dust2Mesh = new Mesh;
+  *dust2Mesh = OBJParser.ParseObj(assetsDir + "/dust2.obj", true);
 
-    // Create the dust2 map world object
-    WorldObject* dust2Wo = WorldObjectManager::NewWorldObject("dust2 map");
-    // Can leave material unset, because it should be set within the mesh itself, tris-by-tris, defined by the mtl file.
-    dust2Wo->AddComponent<Components::MeshRenderer>(
-        dust2Mesh,
-        nullptr
-    );
-    dust2Wo->transform->Scale(Vector3d(1,1,1)* 20);
+  // Create the dust2 map world object
+  WorldObject *dust2Wo = WorldObjectManager::NewWorldObject("dust2 map");
+  // Can leave material unset, because it should be set within the mesh itself,
+  // tris-by-tris, defined by the mtl file.
+  dust2Wo->AddComponent<Components::MeshRenderer>(dust2Mesh, nullptr);
+  dust2Wo->transform->Scale(Vector3d(1, 1, 1) * 20);
 
-    // Create player light
-    WorldObject* cameraWo = WorldObjectManager::FindObjectById("main_camera");
-    if (!cameraWo) {
-        throw std::runtime_error(std::string("Attempted to initialize scene ") + GetSceneName() + " without having a any camera loaded! But that is required!");
-    }
-    WorldObjectManager::NewWorldObject("player-light", cameraWo->transform)
-        ->AddComponent<Components::PointLight>(15, Color(255, 183, 140));
+  // Create player light
+  WorldObject *cameraWo = WorldObjectManager::FindObjectById("main_camera");
+  if (!cameraWo) {
+    throw std::runtime_error(
+        std::string("Attempted to initialize scene ") + GetSceneName() +
+        " without having a any camera loaded! But that is required!");
+  }
+  WorldObjectManager::NewWorldObject("player-light", cameraWo->transform)
+      ->AddComponent<Components::PointLight>(15, Color(255, 183, 140));
 
-    // Position main camera
-    WorldObject* fpsCameraParent = WorldObjectManager::FindObjectById("main_camera_ypiv");
-    if (fpsCameraParent) {
-        fpsCameraParent->transform->SetPosition(Vector3d(-99.3422, 63.5574, 210.063));
-        fpsCameraParent->transform->SetRotation(Vector3d(17.86, 60.58, 15.67));
-    }
+  // Position main camera
+  WorldObject *fpsCameraParent =
+      WorldObjectManager::FindObjectById("main_camera_ypiv");
+  if (fpsCameraParent) {
+    fpsCameraParent->transform->SetPosition(
+        Vector3d(-99.3422, 63.5574, 210.063));
+    fpsCameraParent->transform->SetRotation(Vector3d(17.86, 60.58, 15.67));
+  }
 
-    return;
+  return;
 }
 
-Dust2Scene::~Dust2Scene()
-{
-    delete dust2Mesh;
-    dust2Mesh = nullptr;
+Dust2Scene::~Dust2Scene() {
+  delete dust2Mesh;
+  dust2Mesh = nullptr;
 }
 
-void Dust2Scene::Update(double deltaTime)
-{
-    
+void Dust2Scene::Update(double deltaTime) {
+  if (Input::Keyboard::GetKey(Input::KEY_CODE::ESCAPE)) {
+    Input::Application::Exit();
+  }
 }
-
