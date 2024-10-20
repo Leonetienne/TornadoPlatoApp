4cdbae53cb62a5b196ec2cee8755ece3cac3955e
Leon Etienne <me@leonetienne.de>
2024-10-21 00:14:23 +0200

    Version empty keybinds_local.h

---
+++ Keybinds_local.h
@@ -0,0 +1,5 @@
+// Override default keybinds, if required
+//#define KB_FPSCAM_FORWARD         Input::KEY_CODE::W
+//#define KB_FPSCAM_BACKWARD        Input::KEY_CODE::R
+//#define KB_FPSCAM_STRAFE_LEFT     Input::KEY_CODE::A
+//#define KB_FPSCAM_STRAFE_RIGHT    Input::KEY_CODE::S
