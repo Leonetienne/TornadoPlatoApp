a6d8ed17c831477a3b07f3911cbc3374da4f6751
Leon Etienne <github@leonetienne.de>
2024-10-21 16:27:47 +0200

    fix bindings

--- Keybinds.h
+++ Keybinds.h
@@ -3,12 +3,11 @@
 // Load default, fallback keybinds
 // FPS CAM
 #define KB_FPSCAM_FORWARD         Input::KEY_CODE::W
-#define KB_FPSCAM_BACKWARD        Input::KEY_CODE::A
-#define KB_FPSCAM_STRAFE_LEFT     Input::KEY_CODE::S
+#define KB_FPSCAM_BACKWARD        Input::KEY_CODE::S
+#define KB_FPSCAM_STRAFE_LEFT     Input::KEY_CODE::A
 #define KB_FPSCAM_STRAFE_RIGHT    Input::KEY_CODE::D
 #define KB_FPSCAM_DOWN            Input::KEY_CODE::Q
-#define KB_FPSCAM_UP              Input::KEY_CODE::F
-#define KB_FPSCAM_FORWARD         Input::KEY_CODE::W
+#define KB_FPSCAM_UP              Input::KEY_CODE::E
 
 #define KB_FPSCAM_SPRINT          Input::KEY_CODE::LSHIFT
 #define KB_FPSCAM_DUMP_COORDS     Input::KEY_CODE::Z
